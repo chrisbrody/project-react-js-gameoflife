047111dd30472a107709042f47a401db9126ca95
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-12 08:11:16 -0500

    Updates

---
+++ precache-manifest.e7ba19fb39e0616c87efe0d8e3d4d617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a29c1fe8a25a1d923c17bdfbfe5a1b",
+    "url": "/project-react-js-gameoflife/index.html"
+  },
+  {
+    "revision": "b79575d7ce125c1c6270",
+    "url": "/project-react-js-gameoflife/static/css/main.2bd83607.chunk.css"
+  },
+  {
+    "revision": "bdfae1fd1483becda8bc",
+    "url": "/project-react-js-gameoflife/static/js/2.66abe07d.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/project-react-js-gameoflife/static/js/2.66abe07d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b79575d7ce125c1c6270",
+    "url": "/project-react-js-gameoflife/static/js/main.6154cef6.chunk.js"
+  },
+  {
+    "revision": "05f40d13712ad3c2ad5e",
+    "url": "/project-react-js-gameoflife/static/js/runtime-main.a0a567e8.js"
+  }
+]);
